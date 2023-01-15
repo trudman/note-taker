5e161e472b40c7827c8a89faf927dff102f3d4b1
Tyler Rudman <tyler.rudman@gmail.com>
2023-01-15 17:59:16 -0500

    completed api get and post and routed to server

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,32 @@
 const router = require("express").Router();
-// const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
+const util = require("util");
+const readFromFile = util.promisify(fs.readFile);
 
-router.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../db/db.json"));
+router.get("/", (req, res) => {
+  readFromFile("./db/db.json")
+    .then(JSON.parse)
+    .then((data) => res.json(data));
 });
 
-
-
+router.post("/", (req, res) => {
+  let notesArr = [];
+  let newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    notesArr = JSON.parse(data);
+    notesArr.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notesArr), "utf-8", (err) => {
+      if (err) throw err;
+      console.log("Saved");
+      res.end();
+    });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const htmlRoute = require("./routes/html");
+const apiRoute = require("./routes/api");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/", htmlRoute);
+app.use("/api/notes", apiRoute);
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
