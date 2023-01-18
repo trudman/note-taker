0c0b1d58a6ddc3cd47193a8638cf257940cb740c
Tyler Rudman <tyler.rudman@gmail.com>
2023-01-18 18:16:24 -0500

    added delete and cleared db

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const util = require("util");
 const readFromFile = util.promisify(fs.readFile);
 
+let notesArr = [];
+
 router.get("/", (req, res) => {
   readFromFile("./db/db.json")
     .then(JSON.parse)
@@ -11,7 +13,6 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  let notesArr = [];
   let newNote = {
     id: uuidv4(),
     title: req.body.title,
@@ -24,9 +25,35 @@ router.post("/", (req, res) => {
     fs.writeFile("./db/db.json", JSON.stringify(notesArr), "utf-8", (err) => {
       if (err) throw err;
       console.log("Saved");
-      res.end();
+      res.status(200).json(notesArr);
     });
   });
 });
 
+router.delete("/:id", (req, res) => {
+  let noteID = JSON.parse(fs.readFileSync("./db/db.json"));
+  let delNote = noteID.filter((note) => note.id !== req.params.id);
+  fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+  console.log("Deleted");
+  res.json(delNote);
+});
+
 module.exports = router;
+
+// router.delete("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const deleted = notesArr.find((notesArr) => notesArr.id == id);
+//   if (deleted) {
+//     notesArr = notesArr.filter((notesArr) => notesArr.id !== id);
+//     fs.writeFile("./db/db.json"),
+//       JSON.stringify(notesArr),
+//       "utf-8",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("Saved");
+//         res.status(200).json(notesArr);
+//       };
+//   } else {
+//     res.status(404).json({ message: "Not found" });
+//   }
+// });
